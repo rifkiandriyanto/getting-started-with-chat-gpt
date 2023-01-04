a697339cf4378c8f09e2e52569e697b6a8a21dfe
Rifki Andriyanto <andriyantorifki@gmail.com>
2023-01-04 17:36:35 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send a request to the server with the prompt
+    axios
+      .post("http://localhost:8080/chat", { prompt })
+      .then((res) => {
+        // Update the response state with the server's response
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {console.log("response", response)}
+      <p>{response}</p>
+    </div>
+  );
+}
+
+export default App;
